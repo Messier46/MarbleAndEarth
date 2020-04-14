d07f2d2b72a6d7132ba02945f07af43b2dbf211e
Adam Vickerman <svadam15@gmail.com>
2020-04-14 12:34:20 -0500

    Revert "Setup of models"
    This reverts commit b6c6178a972bbd0f401e54415335ef1076bb4bf6.

---
+++ MarbleAndEarth/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarbleAndEarth.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Image { get; set; }
+		public string Type { get; set; }
+		public decimal Price { get; set; }
+		public string Description { get; set; }
+		public int Qty { get; set; }
+	}
+}
---
+++ MarbleAndEarth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarbleAndEarth.Startup))]
+namespace MarbleAndEarth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
