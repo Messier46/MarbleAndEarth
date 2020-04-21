204d9457931ff6914990818e9a39416cf641d1ff
Adam Vickerman <svadam15@gmail.com>
2020-04-21 11:33:44 -0500

    Added shopping cart
    Basic shopping cart is functional. Need to add removing from the database when an item is selected

---
+++ MarbleAndEarth/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarbleAndEarth.Models
+{
+	public class Item
+	{
+		public Product Product
+		{
+			get;
+			set;
+		}
+		public int Quanity
+		{
+			get;
+			set;
+		}
+	}
+}
